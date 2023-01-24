c4c7cb904b441f09b35c566b462d8628974c713e
Percy Ma <kecrily@gmail.com>
2023-01-24 22:56:08 +0800

    fix: add h1 tag in home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,8 @@ import { site } from '../composables'
 <template>
   <header class="flex items-baseline justify-between">
     <RouterLink to="/" class="font-semibold text-lg">
-      {{ site.title }}
+      <h1 v-if="useRoute().path === '/'">{{ site.title }}</h1>
+      <template v-else>{{ site.title }}</template>
     </RouterLink>
     <a class="ri:rss-fill" href="/feed.xml" target="_blank" title="Subscribe Feed" />
   </header>
