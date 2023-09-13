8c6e66bae34624530325bad9d5a9e7db392bd64b
Percy Ma <kecrily@gmail.com>
2023-09-13 22:15:35 +0800

    feat: auto get width and height

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', '@unocss/nuxt'],
   app: { rootId: '_' },
   content: {
-    highlight: { theme: 'nord' }
+    highlight: { theme: 'nord' },
+    markdown: {
+      rehypePlugins: {
+        'rehype-img-size': { dir: 'public' }
+      }
+    }
   },
   devtools: true,
   experimental: {
