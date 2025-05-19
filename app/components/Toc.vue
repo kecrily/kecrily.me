9f00d1754b04750c67ad1b5a192a95afcf06a1c8
Percy Ma <kecrily@gmail.com>
2025-05-19 22:41:52 +0800

    fix: add dark support for toc  title

--- app/components/Toc.vue
+++ app/components/Toc.vue
@@ -11,7 +11,7 @@ const isShow = !(
 <template>
   <aside v-if="isShow" class="not-prose xl:(fixed top-0 bottom-0 left-0 pt-16 mt-28 pl-12 w-xs)">
     <nav id="toc">
-      <h2 class="text-sm text-neutral-700 font-medium mb-3">目录</h2>
+      <h2 class="text-sm text-neutral-700 dark:text-neutral-300 font-medium mb-3">目录</h2>
       <ul class="text-sm text-neutral-700 border-neutral-200 dark:(text-neutral-300 border-neutral-700) border-l space-y-2">
         <li
           v-for="link in value?.links"
